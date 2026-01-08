d4953f14b6e1076ede26b8436595fce87921d221
Chu A. <chu.amk22@gmail.com>
2026-01-08 14:21:27 +0100

    handling rejections (asynchronous operations)

--- errors/errors.js
+++ errors/errors.js
@@ -1,18 +1,65 @@
 const doTask = (amount) => {
-  if (typeof amount !== "number")
-    throw new TypeError("Amount must be a number");
-  if (amount <= 0) throw new RangeError("Amount must be non-negative");
-  if (amount % 2) throw new OddError("amount");
-  return amount / 2;
+  return new Promise((resolve, reject) => {
+    if (typeof amount !== "number") {
+      reject(new TypeError("Amount must be a number"));
+      return;
+    }
+    if (amount <= 0) {
+      reject(new RangeError("Amount must be greater than zero"));
+      return;
+    }
+    if (amount % 2) {
+      reject(new OddError("amount"));
+      return;
+    }
+    resolve(amount / 2);
+  });
 };
 
 class OddError extends Error {
   constructor(varName = "") {
-    super(`${varName} must be an odd number`);
+    super(`${varName} must be an even number`);
+    this.code = "ERR_AMOUNT_MUST_BE_EVEN";
   }
   get name() {
-    return "OddError";
+    return "OddError " + [this.code];
   }
 }
 
-doTask(3);
+doTask(4)
+  .then((result) => {
+    throw Error("spanner in the works");
+    // console.log("Result", result);
+  })
+  .catch((err) => {
+    if (err.code === "ERR_AMOUNT_MUST_BE_NUMBER") {
+      console.error("Expected a number");
+    } else if (err.code === "ERR_AMOUNT_MUST_EXCEED_ZERO") {
+      console.error("Number must be non-negative");
+    } else if (err.code === "ERR_AMOUNT_MUST_BE_EVEN") {
+      console.error("Number must be even");
+    } else {
+      console.error("unknown error: ", err);
+    }
+  });
+
+// try {
+//   const result = doTask(4);
+//   result();
+//   console.log(" Result: ", result);
+// } catch (err) {
+//   if (err.code === "ERR_AMOUNT_MUST_BE_NUMBER") {
+//     console.error("Expected a number");
+//   } else if (err.code === "ERR_AMOUNT_MUST_EXCEED_ZERO") {
+//     console.error("Number must be non-negative");
+//   } else if (err.code === "ERR_AMOUNT_MUST_BE_EVEN") {
+//     console.error("Number must be even");
+//   } else {
+//     console.error("unknown error: ", err);
+//   }
+// }
+
+// function codify(err, code) {
+//   err.code = code;
+//   return err;
+// }
--- events/events.js
+++ events/events.js
@@ -1,6 +1,6 @@
 // Creating Events
 
-const { EventEmitter } = require("events");
+// const { EventEmitter } = require("events");
 
 // Create an instance of EventEmitter (Creating a new event emitter)
 const myEmitter = new EventEmitter(); // using the new keyword to create an instance of the EventEmitter class
@@ -27,28 +27,28 @@ class MyEmitter extends EventEmitter {
 const emitter = new MyEmitter({ name: "My Emitter" });
 
 // register an event listener for the event "close"
-emitter.on("close", () => console.log("close event fired")); // listen for the close event
-emitter.emit("close"); // emit the close event
+// emitter.on("close", () => console.log("close event fired")); // listen for the close event
+// emitter.emit("close"); // emit the close event
 
-// register an event listener for the event "add"
-emitter.on("add", (a, b) => {
-  console.log(`The sum is: ${a + b}`);
-}); // listen for the add event
+// // register an event listener for the event "add"
+// emitter.on("add", (a, b) => {
+//   console.log(`The sum is: ${a + b}`);
+// }); // listen for the add event
 
-emitter.emit("add", 5, 10); // emit the add event with arguments 5 and 10
+// emitter.emit("add", 5, 10); // emit the add event with arguments 5 and 10
 
-console.log("---- Demonstrating PrependListener ----");
-emitter.on("my-event", () => console.log("1st"));
-emitter.on("my-event", () => console.log("2nd"));
-emitter.prependListener("my-event", () => console.log("3rd"));
+// console.log("---- Demonstrating PrependListener ----");
+// emitter.on("my-event", () => console.log("1st"));
+// emitter.on("my-event", () => console.log("2nd"));
+// emitter.prependListener("my-event", () => console.log("3rd"));
 
-emitter.emit("my-event"); // emit the my-event event
+// emitter.emit("my-event"); // emit the my-event event
 
-console.log("---- Single User Listener (once) ----");
-emitter.once("single-use", () => console.log("This will be logged only once"));
-emitter.emit("single-use"); // This will log the message
-emitter.emit("single-use");
-emitter.emit("single-use");
+// console.log("---- Single User Listener (once) ----");
+// emitter.once("single-use", () => console.log("This will be logged only once"));
+// emitter.emit("single-use"); // This will log the message
+// emitter.emit("single-use");
+// emitter.emit("single-use");
 
 // console.log("---- Removing Listeners ----");
 // const listener1 = () => console.log("Listener 1");
@@ -66,10 +66,32 @@ emitter.emit("single-use");
 // setTimeout(() => emitter.removeAllListeners("removable-event"), 500);
 // setTimeout(() => emitter.removeAllListeners(), 1100);
 
-console.log("---- Error Event Handling ----");
+// console.log("---- Error Event Handling ----");
 
-process.stdin.resume(); // Keep the Node.js process running
+// process.stdin.resume(); // Keep the Node.js process running
 
-emitter.on("error", (err)=>console.log("Error: ", err.message))
+// emitter.on("error", (err)=>console.log("Error: ", err.message))
 
-emitter.emit("error", new Error("Something went wrong!")); // emit an error event without a listener
+// emitter.emit("error", new Error("Something went wrong!")); // emit an error event without a listener
+
+import { once, EventEmitter } from "events";
+import { setTimeout } from "timers/promises";
+
+const sometimesLaggy = new EventEmitter();
+
+const ac = new AbortController();
+const { signal } = ac;
+
+setTimeout(2000 * Math.random(), null, { signal }).then(() =>
+  sometimesLaggy.emit("ping")
+);
+
+setTimeout(500).then(() => ac.abort());
+
+try {
+  await once(sometimesLaggy, "ping", { signal });
+  console.log("Ping!");
+} catch (error) {
+  if (error.code !== "ABORT_ERR") throw error;
+  console.log("canceled");
+}
