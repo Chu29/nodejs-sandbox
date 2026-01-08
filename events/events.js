fd5f4ce550c11e0d49e1769886b68e86a4284aef
Chu A. <chu.amk22@gmail.com>
2026-01-08 11:34:55 +0100

    Removing event listeners using removeLister

--- events/events.js
+++ events/events.js
@@ -29,3 +29,35 @@ const emitter = new MyEmitter({ name: "My Emitter" });
 // register an event listener for the event "close"
 emitter.on("close", () => console.log("close event fired")); // listen for the close event
 emitter.emit("close"); // emit the close event
+
+// register an event listener for the event "add"
+emitter.on("add", (a, b) => {
+  console.log(`The sum is: ${a + b}`);
+}); // listen for the add event
+
+emitter.emit("add", 5, 10); // emit the add event with arguments 5 and 10
+
+console.log("---- Demonstrating PrependListener ----");
+emitter.on("my-event", () => console.log("1st"));
+emitter.on("my-event", () => console.log("2nd"));
+emitter.prependListener("my-event", () => console.log("3rd"));
+
+emitter.emit("my-event"); // emit the my-event event
+
+console.log("---- Single User Listener (once) ----");
+emitter.once("single-use", () => console.log("This will be logged only once"));
+emitter.emit("single-use"); // This will log the message
+emitter.emit("single-use");
+emitter.emit("single-use");
+
+console.log("---- Removing Listeners ----");
+const listener1 = () => console.log("Listener 1");
+const listener2 = () => console.log("Listener 2");
+
+emitter.on("removable-event", listener1);
+emitter.on("removable-event", listener2);
+
+setInterval(() => emitter.emit("removable-event"), 200);
+
+setTimeout(() => emitter.removeListener("removable-event", listener1), 500);
+setTimeout(() => emitter.removeListener("removable-event", listener2), 1100);
