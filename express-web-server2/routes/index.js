d20e88f32798b0e3c10e84e00603c0764d006274
Chu A. <chu.amk22@gmail.com>
2026-01-22 10:54:49 +0100

    development-only static-hosting and serving dynamic content with express

--- express-web-server2/routes/index.js
+++ express-web-server2/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
+const { route } = require("./hello");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
 module.exports = router;
