9dcf5f7fc755e27a5799cd464769d7e86822f398
Chu A. <chu.amk22@gmail.com>
2026-01-26 14:23:00 +0100

    implement CRUD functions to handle create, read update and delete ops

--- my-express-service/model.js
+++ my-express-service/model.js
@@ -9,7 +9,28 @@ function bicycleModel() {
     1: { brand: "Veloretti", color: "green" },
     1: { brand: "Batavus", color: "yellow" },
   };
-  return { read };
+  return { create, read, update, del, uid };
+
+  function uid() {
+    return (
+      Object.keys(db)
+        .sort((a, b) => a - b)
+        .map(Number)
+        .filter((n) => !isNaN(n))
+        .pop() +
+      1 +
+      ""
+    );
+  }
+  function create(id, data, cb) {
+    if (db.hasOwnProperty(id)) {
+      const err = Error("resource exists");
+      setImmediate(() => cb(err));
+      return;
+    }
+    db[id] = data;
+    setImmediate(() => cd(null, id));
+  }
 
   function read(id, cb) {
     if (!db.hasOwnProperty(id)) {
@@ -18,5 +39,13 @@ function bicycleModel() {
       return;
     }
     setImmediate(() => cb(null, db[id]));
+  }
+
+  function update(id, data, cb) {
+    if (!db.hasOwnProperty(id)) {
+      const err = Error("not found");
+      setImmediate(() => cb(err));
+      return;
+    }
   }
 }
