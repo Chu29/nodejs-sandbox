90a660463550db87e57f7f526d456a8a3ed1797b
Chu A. <chu.amk22@gmail.com>
2026-01-29 13:31:43 +0100

    feat: implement getColorsById function and change all the functions to async for better error handling

--- service-aggregation/bicycle-service/services/color.service.js
+++ service-aggregation/bicycle-service/services/color.service.js
@@ -1,7 +1,8 @@
 import db from "../config/db.js";
+import { STATUS_CODES } from "node:http";
 
 /** GET ALL COLORS */
-export const getColors = (req, res, next) => {
+export const getColors = async (req, res, next) => {
   try {
     let query = "SELECT * FROM colors";
 
@@ -28,3 +29,32 @@ export const getColors = (req, res, next) => {
 };
 
 /** GET COLOR BY ID */
+export const getColorById = async (req, res, next) => {
+  const id = req.params.id;
+
+  try {
+    let query = "SELECT * FROM colors WHERE id = ?";
+    const result = db.prepare(query).get(id);
+
+    if (!result) {
+      let error = new Error(STATUS_CODES[404].message);
+      error.status = 404;
+      throw error;
+    }
+
+    //Add HATEOS Links
+    const color = {
+      ...result,
+      _links: {
+        self: { href: `/colors/${result.id}` },
+        collection: { href: "/colors" },
+      },
+    };
+    console.log(color);
+
+    // return the final data
+    res.json(color);
+  } catch (error) {
+    next(error);
+  }
+};
