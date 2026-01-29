bf7b726fc03d415ee137e438ba467902cc658987
Chu A. <chu.amk22@gmail.com>
2026-01-29 16:16:54 +0100

    fix: update import statements and ensure proper file extensions for middleware and services

--- service-aggregation/bicycle-service/routes/color.js
+++ service-aggregation/bicycle-service/routes/color.js
@@ -1,6 +1,19 @@
 import { Router } from "express";
-import validateContentType from "../middleware/validateContentType";
+import validateContentType from "../middleware/validateContentType.js";
+import {
+  createColor,
+  getColorById,
+  getColors,
+  updateColor,
+} from "../services/color.service.js";
 const router = Router();
 
 /* validateContentType middleware */
 router.use(validateContentType);
+
+router.get("/", getColors);
+router.get("/:id", getColorById);
+router.post("/", createColor);
+router.put("/:id", updateColor);
+
+export default router;
