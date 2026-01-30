105d3087c9ce82c9657150e5790ab9e41caf81c8
Chu A. <chu.amk22@gmail.com>
2026-01-30 15:12:54 +0100

    feat: implement bicycle and brand fetching logic in API route

--- service-aggregation/api/routes/index.js
+++ service-aggregation/api/routes/index.js
@@ -1,9 +1,52 @@
 import { Router } from "express";
+import createError from "http-errors";
 const router = Router();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.json({ message: "Welcome to the Service Aggregation API" });
+const { BICYCLE_SERVICE_PORT = 4040, BRAND_SERVICE_PORT = 5050 } = process.env;
+
+const bicycleService = `http://localhost:${BICYCLE_SERVICE_PORT}`;
+const brandService = `http://localhost:${BRAND_SERVICE_PORT}`;
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+
+  const noop = Function.prototype;
+  const signal = AbortSignal.timeout(5000);
+
+  const bicycleReq = await fetch(`${bicycleService}/colors/${id}`, {
+    signal,
+  });
+
+  const brandReq = await fetch(`${brandService}/brands/${id}`, {
+    signal,
+  });
+
+  if (bicycleReq.status === 404 || brandReq.status === 404) {
+    next(createError(404, "Bicycle or Brand not found"));
+  }
+
+  if (bicycleReq.status === 400 || brandReq.status === 400) {
+    next(createError(400, "Bad Request to Bicycle or Brand service"));
+  }
+
+  const bicycleProm = bicycleReq.json();
+  const brandProm = brandReq.json();
+
+  bicycleProm.catch(noop);
+  brandProm.catch(noop);
+
+  const results = await Promise.allSettled([bicycleProm, brandProm]);
+
+  for (const { reason } of results) {
+    if (reason) console.log(reason);
+  }
+
+  const [bicycle, brand] = results.map(({ value }) => value);
+
+  if (bicycle && brand) {
+    res.setHeader("Content-Type", "application/json");
+    res.send({ id: bicycle.id, color: bicycle.color, brand: brand.brand });
+  }
 });
 
 export default router;
