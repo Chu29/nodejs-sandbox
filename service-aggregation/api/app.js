59a91310af6ed2d95ef53214fd3fbc7fc4afec42
Chu A. <chu.amk22@gmail.com>
2026-01-30 12:29:33 +0100

    chore(setup): initial commit: api-service

---
+++ service-aggregation/api/app.js
@@ -0,0 +1,33 @@
+import createError from "http-errors";
+import express, { json, urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+
+import indexRouter from "./routes/index.js";
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  const statusCode = err.status || 500;
+  res.status(statusCode);
+  res.json({
+    status: statusCode,
+    message: err.message,
+    error: req.app.get("env") === "development" ? err.stack : {},
+  });
+});
+
+export default app;
