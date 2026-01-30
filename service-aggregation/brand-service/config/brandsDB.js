a692f7de6ed3cb5b8708e796824241bc399e11b7
Chu A. <chu.amk22@gmail.com>
2026-01-30 12:22:22 +0100

    feat: configure brands database to use file storage

--- service-aggregation/brand-service/config/brandsDB.js
+++ service-aggregation/brand-service/config/brandsDB.js
@@ -1,16 +1,17 @@
 import { DatabaseSync } from "node:sqlite";
 
-const db = new DatabaseSync(":memory:");
+// const db = new DatabaseSync(":memory:");
+const db = new DatabaseSync(`${import.meta.dirname}/brands.db`);
 
 db.exec(`
-  CREATE TABLE brands (
+  CREATE TABLE IF NOT EXISTS brands (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     brand TEXT NOT NULL
   ) STRICT
 `);
 
-db.exec(`
-  INSERT INTO brands (brand) VALUES ('Gazelle'), ('Batavus'), ('Azor'), ('Cortina'), ('Giant'), ('Sparta')
-  `);
+// db.exec(`
+//   INSERT OR IGNORE INTO brands (brand) VALUES ('Gazelle'), ('Batavus'), ('Azor'), ('Cortina'), ('Giant'), ('Sparta')
+//   `);
 
 export default db;
