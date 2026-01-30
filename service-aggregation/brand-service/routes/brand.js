a2b4bcec3e8ecc0313ad00066353721dd0595514
Chu A. <chu.amk22@gmail.com>
2026-01-30 10:27:05 +0100

    feat: add createBrand endpoint to brand routes

--- service-aggregation/brand-service/routes/brand.js
+++ service-aggregation/brand-service/routes/brand.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import validateContentType from "../../middleware/validateContentType.mjs";
-import { getBrandById, getBrands } from "../services/brand.service.js";
+import {
+  createBrand,
+  getBrandById,
+  getBrands,
+} from "../services/brand.service.js";
 
 const router = Router();
 
 router.use(validateContentType);
 router.get("/", getBrands);
 router.get("/:id", getBrandById);
+router.post("/", createBrand);
 export default router;
