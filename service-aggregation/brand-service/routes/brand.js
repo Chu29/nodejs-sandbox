86ac5b92bd343147ac122dc9e5c0479311bdd9fc
Chu A. <chu.amk22@gmail.com>
2026-01-30 10:12:07 +0100

    feat: implement brand routes for fetching brands and brand by ID

--- service-aggregation/brand-service/routes/brand.js
+++ service-aggregation/brand-service/routes/brand.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import validateContentType from "../../middleware/validateContentType.mjs";
+import { getBrandById, getBrands } from "../services/brand.service.js";
 
 const router = Router();
 
 router.use(validateContentType);
+router.get("/", getBrands);
+router.get("/:id", getBrandById);
+export default router;
