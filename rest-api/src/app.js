ca732d55f8d8ea19e9695c9f880848f6ce3aa3d5
Chu A. <chu.amk22@gmail.com>
2026-02-09 14:00:01 +0100

    feat: implement error handling and graceful shutdown in Express app

--- rest-api/src/app.js
+++ rest-api/src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
-import { connectDB } from "./src/db.js";
+import { connectDB, disconnectDB } from "./src/db.js";
 
 import movieRouter from "./routes/movieRoutes.js";
 
@@ -14,8 +14,37 @@ app.use("/movies", movieRouter);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on localhost:${PORT}`);
 });
 
-// Handle 
+/**
+ * Let's handle some common edge cases that may occur and cause the application to crash or behave unexpectedly.
+ */
+
+// 1. Handle unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Rejection", err);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1);
+  });
+});
+
+// 2. Handle uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception", err);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1);
+  });
+});
+
+// 3. Gracefully shutdown
+process.on("SIGTERM", async () => {
+  console.log("SIGTERM received, shutting down gracefully");
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(0);
+  });
+});
