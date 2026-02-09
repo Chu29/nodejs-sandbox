6f4700bbbb20affc3236927fd6eefcc1a684737c
Chu A. <chu.amk22@gmail.com>
2026-02-09 11:21:18 +0100

    feat: initialize Express app and configure dotenv and database connection

--- rest-api/src/app.js
+++ rest-api/src/app.js
@@ -1,9 +1,21 @@
 import express from "express";
+import { config } from "dotenv";
+import { connectDB } from "./src/db.js";
+
+import movieRouter from "./routes/movieRoutes.js";
+
+config();
+connectDB();
 
 const app = express(); // Initialize Express application
+
+// Register movieRouter
+app.use("/movies", movieRouter);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on localhost:${PORT}`);
 });
+
+// Handle 
