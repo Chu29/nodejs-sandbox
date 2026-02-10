603fbc7c205731b5ff691e248b1957c72d1e3fac
Chu A. <chu.amk22@gmail.com>
2026-02-10 10:01:11 +0100

    feat: add login route to authentication module

--- rest-api/src/modules/auth/auth.routes.js
+++ rest-api/src/modules/auth/auth.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { register } from "./auth.controller.js";
+import { login, register } from "./auth.controller.js";
 const router = Router();
 
 // create a new user
 router.post("/register", register);
+
+// user login
+router.post("/login", login);
 
 export default router;
