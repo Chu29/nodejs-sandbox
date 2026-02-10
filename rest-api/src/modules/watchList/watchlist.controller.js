5d2dc03c3c3db0584467921db8c98279466e575f
Chu A. <chu.amk22@gmail.com>
2026-02-10 14:30:48 +0100

    feat: implement addToWatchList function for managing user watchlist

---
+++ rest-api/src/modules/watchList/watchlist.controller.js
@@ -0,0 +1,38 @@
+import { prisma } from "../../src/db.js";
+
+const addToWatchList = async (req, res) => {
+  const { movieId, userId, notes, rating, status } = req.body;
+  // check if movie exists
+  const movie = await prisma.movies.findUnique({ where: { id: movieId } });
+
+  if (!movie) {
+    return res.status(404).json({ message: "Movie not found" });
+  }
+
+  // check if movie has already been added
+  const existingEntry = await prisma.watchlistItems.findUnique({
+    where: { userId_movieId: { userId: userId, movieId: movieId } },
+  });
+
+  if (existingEntry) {
+    return res.status(400).json({ message: "Movie already in watchlist" });
+  }
+
+  // create watchlist item
+  const watchlistItem = await prisma.watchlistItems.create({
+    data: {
+      userId,
+      movieId,
+      notes,
+      rating,
+      status: status || "PLANNED",
+    },
+  });
+
+  res.status(201).json({
+    status: "success",
+    data: { watchlistItem },
+  });
+};
+
+export { addToWatchList };
