fa2b2f22076f74a5eac50fb4d1b40200fac3778f
Chu A. <chu.amk22@gmail.com>
2026-06-01 15:13:48 +0100

    feat: add documentation for JavaScript primitive types

---
+++ _nodejs/js-concepts/index.js
@@ -0,0 +1,10 @@
+/**
+ * In javascript, there are 7 primitive types.
+ * 1. Null: null
+ * 2. Undefined: undefined
+ * 3. Number: 42, 1.5, -1e4, NaN
+ * 4. BigInt: 1n, 900719925470993n
+ * 5. String: "hello", 'world'
+ * 6. Boolean: true, false
+ * 7. Symbol: Symbol('description'), Symbol.for('key')
+ */
