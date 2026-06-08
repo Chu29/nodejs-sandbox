29003851fb294c807f15e7d53814917671b33351
Chu A. <chu.amk22@gmail.com>
2026-06-08 10:57:47 +0100

    build(deps): create 3 files and update 1 file - Creating a rudimentary node server with the built -in http core module

--- servers/http-server/index.js
+++ servers/http-server/index.js
@@ -1,5 +1,6 @@
 "use strict";
-const http = require("http");
+import { createServer, STATUS_CODES } from "http";
+import url from "node:url";
 const PORT = process.env.PORT || 3000;
 
 const helloFromServer = `<!doctype html>
@@ -14,10 +15,42 @@ const helloFromServer = `<!doctype html>
   </body>
 </html>`;
 
-const server = http.createServer((req, res) => {
+const root = `<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <a href="/hello">Home Page</a>
+  </body>
+</html>`;
+
+const server = createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
-  res.end(helloFromServer);
+  if (req.method !== "GET") {
+    res.statusCode = 405;
+    res.end(STATUS_CODES[res.statusCode] + "\r\n");
+    return;
+  }
+
+  const { pathname } = url.parse(req.url);
+  //check if the route is / then return the root
+  if (pathname === "/") {
+    res.end(root);
+    return;
+  }
+  //check if the route is /hello then return the helloFromServer
+  if (pathname === "/hello") {
+    res.end(helloFromServer);
+    return;
+  }
+
+  //if the route is not / or /hello then return 404
+  res.statusCode = 404;
+  res.end(STATUS_CODES[res.statusCode] + "\r\n");
 });
 
-console.log(`Server listening on PORT: ${PORT}`);
+console.log(`Server listening on PORT: http://localhost:${PORT}`);
 server.listen(PORT);
